b73e2a27781632336a21d1d018b05b263f016c79
Colin Rood <crrood@gmail.com>
2017-03-01 16:37:34 +1100

    refine plotting, change data generation

--- linear_regression.py
+++ linear_regression.py
@@ -3,17 +3,15 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
 # create semi-random data
-sq_footage = np.arange(1000, 3000, 100)
-sq_footage = np.round(sq_footage + np.random.randn(sq_footage.size) * 300)
+M = 50
+sq_footage = np.round(500 + np.random.rand(M) * 2500)
+num_bedrooms = np.round(1 + np.random.rand(M) * 3)
+crime_rate = np.round(0 + np.random.rand(M) * 150)
 
-num_bedrooms = np.arange(1, 3, .1)
-num_bedrooms = np.round(num_bedrooms + np.random.randn(num_bedrooms.size))
-num_bedrooms += (num_bedrooms < 1) + 0
+X = np.array([np.ones(sq_footage.size), sq_footage, num_bedrooms, crime_rate]).T
 
-X = np.array([np.ones(sq_footage.size), sq_footage, num_bedrooms]).T
-
-# based around the line f(X) = 500 + 2X[1] + 1000X[2]
-theta_true = np.array([500, 2, 1000])
+# create dependent data
+theta_true = np.array([3000, .6, 500, -10])
 Y = np.matmul(X, theta_true)
 Y = np.round(Y + np.random.randn(Y.size) * np.average(Y) / 10)
 
@@ -26,8 +24,8 @@ X_scaled[:,0] = np.ones(X_scaled[:,0].size)
 # print(Y)
 # print(X_scaled)
 #
-# for i in range(1, X[0,:].size):
-# 	plt.subplot(X[0,:].size - 1, 1, i)
+# for i in range(1, X[0].size):
+# 	plt.subplot(X[0].size - 1, 1, i)
 # 	plt.plot(X[:,i], Y, 'ro')
 # plt.show()
 
@@ -41,10 +39,10 @@ def dJ(theta):
 
 # variables for gradient descent
 ALPHA = 0.001
-epsilon_limits = np.array([0.001, 0.1, 1.])
+epsilon_limits = np.array([0.1, 0.3, 1.])
 SMALLEST_EPSILON_LIMIT = np.min(epsilon_limits)
 epsilon = 100. # initialized to an arbitrary value greater than EPSILON_LIMIT
-theta, theta_delta = np.zeros(X_scaled[0,:].size), np.zeros(X_scaled[0,:].size)
+theta, theta_delta = np.zeros(X_scaled[0].size), np.zeros(X_scaled[0].size)
 theta_results = np.zeros(epsilon_limits.size * theta.size).reshape(epsilon_limits.size, theta.size)
 
 i = 0
@@ -83,8 +81,9 @@ print("theta_results:\n", theta_results)
 
 # check for convergence
 # plt.plot(range(i), j_history[:i])
-# plt.plot(range(20), theta_history[:20,0], range(20), theta_history[:20,1])
-# plt.show()
+for j in range(theta_history[0].size):
+	plt.plot(range(i), theta_history[:i,j])
+plt.show()
 
 # compute prediction errors
 errors = np.matmul(X_scaled, theta) - Y
@@ -97,8 +96,8 @@ print("total error: ", np.sum(abs(errors)))
 for i in range(1, X[0].size):
 	plt.subplot(X[0].size - 1, 1, i)
 	for j in range(theta_results[:,0].size):
-		plt.plot([np.min(X_scaled[:,i]), np.max(X_scaled[:,i])], 
-			[np.min(np.matmul(X_scaled, theta_results[j])), 
-			np.max(np.matmul(X_scaled, theta_results[j]))])
-	plt.plot(X_scaled[:,i], Y, 'ro')
+		plt.plot([np.min(X[:,i]), np.max(X[:,i])], 
+			[np.min(X_scaled[:,i]) * theta_results[j][i] + theta_results[j][0], 
+			np.max(X_scaled[:,i]) * theta_results[j][i] + theta_results[j][0]])
+	plt.plot(X[:,i], Y, 'r.')
 plt.show()
