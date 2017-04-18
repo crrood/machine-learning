a9f23188e206fe5f045f55a8fb2364935b3370eb
colin <crrood@gmail.com>
2017-04-18 11:47:17 -0700

    update gradient checking and add manual weights for testing

--- neural-network.py
+++ neural-network.py
@@ -43,6 +43,8 @@ def train(x, y, params):
 	for i in range(0, NUM_LAYERS - 1):
 		theta[i] = np.random.rand(params["num_nodes"][i + 1], params["num_nodes"][i] + 1) - .5
 
+	theta = [np.array([[3., -2., -2.], [-1., 2., 2.]]), np.array([[-1., 2., 2.]])]
+
 	# variables to hold intermediate values
 	theta_grad = [0] * NUM_LAYERS
 	a = [0] * NUM_LAYERS
@@ -53,48 +55,48 @@ def train(x, y, params):
 	a[0] = np.insert(x, 0, 1, axis=1)
 	error = 100
 	num_iters = 0
-	MAX_ITERS = 3
+	MAX_ITERS = 5
 
 	# gradient checking, to be disabled once the backpropagation algorithm is confirmed to be working
 	GRADIENT_CHECKING = True
 	epsilon = .0001
 
-	if GRADIENT_CHECKING:
-
-		# convert theta to a vector for easier manipulation
-		theta_unrolled = unroll(theta)
-		
-		# initialize lists
-		N = theta_unrolled.size
-		theta_plus_unrolled = [0] * N
-		theta_minus_unrolled = [0] * N
-		theta_plus = [0] * N
-		theta_minus = [0] * N
-
-		a_plus = [0] * N
-		a_minus = [0] * N
-
-		# create matrices
-		for i in range(0, N):
-
-			# add or subtract epsilon from unrolled theta
-			theta_plus_unrolled[i] = np.copy(theta_unrolled)
-			theta_plus_unrolled[i][i] += epsilon
-			theta_minus_unrolled[i] = np.copy(theta_unrolled)
-			theta_minus_unrolled[i][i] -= epsilon
-
-			# prepare node matrices
-			a_plus[i] = [0] * NUM_LAYERS
-			a_minus[i] = [0] * NUM_LAYERS
-			a_plus[i][0] = np.insert(x, 0, 1, axis=1)
-			a_minus[i][0] = np.insert(x, 0, 1, axis=1)
-
-			# convert theta_plus and theta_minus back to matrices for later use
-			theta_plus[i] = reroll(theta_plus_unrolled[i])
-			theta_minus[i] = reroll(theta_minus_unrolled[i])
-
 	# train the NN
 	while error > params["error_limit"] and num_iters < MAX_ITERS:
+
+		if GRADIENT_CHECKING:
+
+			# convert theta to a vector for easier manipulation
+			theta_unrolled = unroll(theta)
+			
+			# initialize lists
+			N = theta_unrolled.size
+			theta_plus_unrolled = [0] * N
+			theta_minus_unrolled = [0] * N
+			theta_plus = [0] * N
+			theta_minus = [0] * N
+
+			a_plus = [0] * N
+			a_minus = [0] * N
+
+			# create matrices
+			for i in range(0, N):
+
+				# add or subtract epsilon from unrolled theta
+				theta_plus_unrolled[i] = np.copy(theta_unrolled)
+				theta_plus_unrolled[i][i] += epsilon
+				theta_minus_unrolled[i] = np.copy(theta_unrolled)
+				theta_minus_unrolled[i][i] -= epsilon
+
+				# prepare node matrices
+				a_plus[i] = [0] * NUM_LAYERS
+				a_minus[i] = [0] * NUM_LAYERS
+				a_plus[i][0] = np.insert(x, 0, 1, axis=1)
+				a_minus[i][0] = np.insert(x, 0, 1, axis=1)
+
+				# convert theta_plus and theta_minus back to matrices for later use
+				theta_plus[i] = reroll(theta_plus_unrolled[i])
+				theta_minus[i] = reroll(theta_minus_unrolled[i])
 
 		# propagate forward
 		for i in range(1, NUM_LAYERS):
@@ -129,7 +131,7 @@ def train(x, y, params):
 					(2 * epsilon))
 
 		# calculate error for output layer
-		d[NUM_LAYERS - 1] = (h - y) * sigmoid_gradient(z[NUM_LAYERS - 1])
+		d[NUM_LAYERS - 1] = (h - y)
 
 		# propagate backward
 		for i in range(NUM_LAYERS - 2, -1, -1):
@@ -139,18 +141,22 @@ def train(x, y, params):
 			theta_grad[i] = np.matmul(d[i + 1].T, a[i]) / M
 
 			# regularize theta gradients and cost
-			theta_grad[i] += np.insert(theta[i][:, 1:], 0, 0, axis=1) * params["lambda"]
+			theta_grad[i] += np.insert(theta[i][:, 1:], 0, 0, axis=1) * params["lambda"] / M
 			cost += (params["lambda"] / (2 * M)) * np.sum(theta[i] ** 2)
 
 			# update weights
 			theta[i] -= theta_grad[i]
 
+		# use gradient checking to train weights
+		# theta_unrolled = unroll(theta)
+		# theta_unrolled -= grad_approx
+		# theta = reroll(theta_unrolled)
+
 		print("------")
 		if GRADIENT_CHECKING:
 			print(grad_approx)
-			print(theta_grad)
+			print(unroll(theta_grad))
 		print(cost)
-		print(theta)
 		print(h)
 		print("------")
 
@@ -180,6 +186,9 @@ def test(x, y, theta, params):
 		if i < NUM_LAYERS - 1:
 			a[i] = np.insert(a[i], 0, 1, axis=1)
 
+	print(theta[0])
+	print(a[1])
+
 	return a[NUM_LAYERS - 1]
 
 # create data to use
@@ -205,4 +214,15 @@ params = {
 x = np.array([[1, 1, 0, 0], [1, 0, 1, 0]]).T
 y = np.array([0, 1, 1, 0]).reshape(4, 1)
 
+# test with random starting weights and backpropagation
 print(test(x, y, train(x, y, params), params))
+
+# manually weighted XOR network
+# print(test(x, y, [np.array([[3, -2, -2], [-1, 2, 2]]), np.array([-1, 2, 2])], params = {
+# 	"num_layers": 3, # including input and output
+# 	"num_nodes": {
+# 		0: 2,	# number of characteristics in data i.e. nodes in input layer
+# 		1: 2,	# number of nodes in hidden layer(s)
+# 		2: 1	# number of categories i.e. nodes in output layer
+# 	}
+# }))
